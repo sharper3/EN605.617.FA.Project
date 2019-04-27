672664dd4df2f6cfdc2f5542f26153e49b1c514b
Stephen Harper <saharper3@gmail.com>
2019-04-27 02:11:59 -0400

    updated readme and bmp class files

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# EN605.617.FA.Project
+# Stephen Harper - EN605.617.FA.Project
+
+## Introduction
+
+The purpose of this project is to take a bitmap image and remove all color except for black, white, and either blue, red, or green, based on the user's choice.  [Example here](https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2009/07/lightroom-highlight-color-step5.jpg?ssl=1)
+
+This is a work in progress.  
+
+## Tasks
+
+### Main Project
+
+1. ~~Basic CUDA program scaffolding.~~
+2. ~~BMP class structure.~~
+3. ~~Load bitmap image data.~~
+4. ~~Output bitmap image information.~~
+5. ~~Host to device memory allocation.~~
+6. Kernel code.
+7. Write modified image data to file.
+8. Kernel timing.
+
+
+### Stretch Goals
+
+9. Host method for same operation for comparison purposes.
+10. Add ability to choose "mixed" colors such as purple, orange, yellow, etc.
--- bmp.h
+++ bmp.h
@@ -1,111 +1,49 @@
-
+#pragma once
 #include <fstream>
 #include <vector>
+#include <iostream>
 #include <stdexcept>
 
-struct BMPFileHeader {
-    uint16_t Filetype;          
-    uint32_t FileSize;          
-    uint16_t Reserved1;          
-    uint16_t Reserved2;          
-    uint32_t OffsetData;        
+struct BitMapFileHeader {
+	unsigned short Filetype;
+	unsigned int FileSize;
+	unsigned short Reserved1;
+	unsigned short Reserved2;
+	unsigned int OffsetData;
 };
 
-struct BMPInfoHeader {
-    uint32_t BSize;                      
-    int32_t Width;                      
-    int32_t Height;                     
-    uint16_t Planes;                    
-    uint16_t BitCount;                 
-    uint32_t Compression;               
-    uint32_t SizeImage;                
-    int32_t XPixelsPerMeter;
-    int32_t YPixelsPerMeter;
-    uint32_t ColorsUsed;               
-    uint32_t ColorsImportant;   
+struct BitMapInfoHeader {
+	unsigned int BSize;
+	int Width;
+	int Height;
+	unsigned short Planes;
+	unsigned short BitCount;
+	unsigned int Compression;
+	unsigned int SizeImage;
+	int XPixelsPerMeter;
+	int YPixelsPerMeter;
+	unsigned int ColorsUsed;
+	unsigned int ColorsImportant;
 };
 
-struct BMPColorHeader {
-    uint32_t RedMask;         // Bit mask for the red channel
-    uint32_t GreenMask;       // Bit mask for the green channel
-    uint32_t BlueMask;        // Bit mask for the blue channel
-    uint32_t AlphaMask;       // Bit mask for the alpha channel
-    uint32_t ColorSpaceType; // Default "sRGB" (0x73524742)
-    uint32_t unused[16];                // Unused data for sRGB color space
-};
+class BMP
+{
+public:
 
-struct BMP {
-    BMPFileHeader FileHeader;
-    BMPInfoHeader BMPInfoHeader;
-    BMPColorHeader BMPColorHeader;
-    std::vector<char> Data;
+	unsigned char** reds;
+	unsigned char** greens;
+	unsigned char** blues;
+	int rows;
+	int cols;
 
-    BMP(const char *fname) {
-        std::cout << "reading file";
-        read(fname);
-    }
+	std::vector<char> buffer;
+	std::vector<char> ColorData;
 
-    void read(const char *fname) {
-    	std::ifstream inp(fname, std::ios_base::binary);
-        inp.read((char*)&FileHeader, sizeof(FileHeader));
-        inp.read((char*)&BMPInfoHeader, sizeof(BMPInfoHeader));
-        inp.read((char*)&BMPColorHeader, sizeof(BMPColorHeader));
-    }
+	BitMapFileHeader FileHeader;
+	BitMapInfoHeader InfoHeader;
 
-    void write(const char *fname) {
-    	// ...
-    }
-
-    void outPutBMPInformation()
-    {
-        outPutFileHeaderInfo();
-        outPutInfoHeaderInfo();
-        outPutColorHeaderInfo();
-    }
-
-    void outPutFileHeaderInfo()
-    {
-        std::cout << "File Header"<< std::endl;
-        std::cout << "_____________________________________"<< std::endl;
-        std::cout << "Filetype: " << FileHeader.Filetype << std::endl;
-        std::cout << "FileSize: " << FileHeader.FileSize << std::endl;
-        std::cout << "Reserved1: " << FileHeader.Reserved1 << std::endl;
-        std::cout << "Reserved2: " << FileHeader.Reserved2 << std::endl;
-        std::cout << "OffsetData: " << FileHeader.OffsetData << std::endl;
-        std::cout << std::endl;
-    }
-
-    void outPutInfoHeaderInfo()
-    {
-        std::cout << "Info Header"<< std::endl;
-        std::cout << "_____________________________________"<< std::endl;
-        std::cout << "BSize: " << BMPInfoHeader.BSize << std::endl;
-        std::cout << "Width: " << BMPInfoHeader.Width << std::endl;
-        std::cout << "Height: " << BMPInfoHeader.Height << std::endl;
-        std::cout << "Planes: " << BMPInfoHeader.Planes << std::endl;
-        std::cout << "BitCount: " << BMPInfoHeader.BitCount << std::endl;
-        std::cout << "Compression: " << BMPInfoHeader.Compression << std::endl;
-        std::cout << "SizeImage: " << BMPInfoHeader.SizeImage << std::endl;
-        std::cout << "XPixelsPerMeter: " << BMPInfoHeader.XPixelsPerMeter << std::endl;
-        std::cout << "YPixelsPerMeter: " << BMPInfoHeader.YPixelsPerMeter << std::endl;
-        std::cout << "ColorsUsed: " << BMPInfoHeader.ColorsUsed << std::endl;
-        std::cout << "ColorsImportant: " << BMPInfoHeader.ColorsImportant << std::endl;
-        std::cout << std::endl; 
-    }
-
-    void outPutColorHeaderInfo()
-    {
-        std::cout << "Color Header"<< std::endl;
-        std::cout << "_____________________________________"<< std::endl;
-        std::cout << "RedMask: " << BMPColorHeader.RedMask << std::endl;
-        std::cout << "GreenMask: " << BMPColorHeader.GreenMask << std::endl;
-        std::cout << "BlueMask: " << BMPColorHeader.BlueMask << std::endl;
-        std::cout << "AlphaMask: " << BMPColorHeader.AlphaMask << std::endl;
-        std::cout << "ColorSpaceType: " << BMPColorHeader.ColorSpaceType << std::endl;
-        std::cout << std::endl; 
-
-    }
-
-private:
-    // ...
+	BMP();
+	void LoadFromFile(char*);
+	void OutputFileData();
+	int WriteImageToFile(char*);
 };
